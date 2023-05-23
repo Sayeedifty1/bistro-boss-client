1e714ba5cc06086a178f6c1e08aeac5e62dccb18
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-23 17:32:53 +0600

    chefService section added

---
+++ src/Pages/Home/ChefService/ChefService.jsx
@@ -0,0 +1,17 @@
+import './ChefService.css'
+
+const ChefService = () => {
+    return (
+        <div className="service-bg bg-fixed ">
+            <div className="  w-[1080px] m-auto bg-white py-16 px-26 text-center">
+                <h1 className='text-4xl mb-6'>Bistro Boss</h1>
+                
+                <p className='px-32'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ad quasi soluta molestiae vero est, ea vitae voluptates tenetur, ducimus rerum praesentium error consequatur sunt id porro ab aspernatur rem explicabo quo ratione laboriosam eos?. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, voluptates!</p>
+                
+            </div>
+        </div>
+
+    );
+};
+
+export default ChefService;
