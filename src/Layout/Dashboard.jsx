25e13befe8eb0701a27a6d51b5fd89d9f7e0077c
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-29 17:14:43 +0600

    admin created and delete user

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,16 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUsers } from 'react-icons/fa';
+import {ImSpoonKnife} from 'react-icons/im';
+import {GoThreeBars} from 'react-icons/go';
+import {AiFillBook} from 'react-icons/ai';
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // TODO: load data from server and check if user is admin
+    const isAdmin = true;
+
     return (
         <div className="drawer drawer-mobile ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -16,6 +23,18 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80">
 
+                    {
+                        isAdmin ?
+                            <>
+                                <li><NavLink to="/dashboard/adminhome"><FaHome></FaHome> Admin Home</NavLink></li>
+                                <li><NavLink to="/dashboard/reservations"><ImSpoonKnife></ImSpoonKnife> Add Items</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><GoThreeBars></GoThreeBars> Manage Items</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><AiFillBook></AiFillBook> Manage Bookings</NavLink></li>
+                                <li><NavLink to="/dashboard/users"><FaUsers></FaUsers> All Users</NavLink></li>
+                               
+                            </>
+                :
+                <>
                     <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
                     <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
                     <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
@@ -25,14 +44,18 @@ const Dashboard = () => {
                         </NavLink>
 
                     </li>
-                    <div className="divider"></div>
-                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
-                    <li><NavLink to="/menu"> Our Menu</NavLink></li>
-                    <li><NavLink to="/order/salad">Order Food</NavLink></li>
-                </ul>
 
-            </div>
+                </>
+                    }
+                <div className="divider"></div>
+                <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
+                <li><NavLink to="/menu"> Our Menu</NavLink></li>
+                <li><NavLink to="/order/salad">Order Food</NavLink></li>
+
+            </ul>
+
         </div>
+        </div >
     );
 };
 
